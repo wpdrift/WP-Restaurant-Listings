0b221d1d164862b7fdf0770054136380beb8705c
Mithun Biswas <bhoot.biswas@gmail.com>
2019-08-27 09:24:18 +0600

    Fix: Undefined variable: restaurant_image_gallery

--- includes/admin/class-wp-restaurant-listings-meta-box-gallery.php
+++ includes/admin/class-wp-restaurant-listings-meta-box-gallery.php
@@ -26,40 +26,41 @@ class WP_Restaurant_Listings_Meta_Box_Gallery {
 		<div id="restaurant_images_container">
 			<ul class="restaurant_images">
 				<?php
-					if ( metadata_exists( 'post', $post->ID, '_restaurant_image_gallery' ) ) {
-						$restaurant_image_gallery = get_post_meta( $post->ID, '_restaurant_image_gallery', true );
-					}
+				$restaurant_image_gallery = '';
+				if ( metadata_exists( 'post', $post->ID, '_restaurant_image_gallery' ) ) {
+					$restaurant_image_gallery = get_post_meta( $post->ID, '_restaurant_image_gallery', true );
+				}
 
-					$attachments         = array_filter( explode( ',', $restaurant_image_gallery ) );
-					$update_meta         = false;
-					$updated_gallery_ids = array();
+				$attachments         = array_filter( explode( ',', $restaurant_image_gallery ) );
+				$update_meta         = false;
+				$updated_gallery_ids = array();
 
-					if ( ! empty( $attachments ) ) {
-						foreach ( $attachments as $attachment_id ) {
-							$attachment = wp_get_attachment_image( $attachment_id, 'thumbnail' );
+				if ( ! empty( $attachments ) ) {
+					foreach ( $attachments as $attachment_id ) {
+						$attachment = wp_get_attachment_image( $attachment_id, 'thumbnail' );
 
-							// if attachment is empty skip
-							if ( empty( $attachment ) ) {
-								$update_meta = true;
-								continue;
-							}
-
-							echo '<li class="image" data-attachment_id="' . esc_attr( $attachment_id ) . '">
-								' . $attachment . '
-								<ul class="actions">
-									<li><a href="#" class="delete tips" data-tip="' . esc_attr__( 'Delete image', 'wp-restaurant-listings' ) . '">' . __( 'Delete', 'wp-restaurant-listings' ) . '</a></li>
-								</ul>
-							</li>';
-
-							// rebuild ids to be saved
-							$updated_gallery_ids[] = $attachment_id;
+						// if attachment is empty skip
+						if ( empty( $attachment ) ) {
+							$update_meta = true;
+							continue;
 						}
 
-						// need to update restaurant meta to set new gallery ids
-						if ( $update_meta ) {
-							update_post_meta( $post->ID, '_restaurant_image_gallery', implode( ',', $updated_gallery_ids ) );
-						}
+						echo '<li class="image" data-attachment_id="' . esc_attr( $attachment_id ) . '">
+									' . $attachment . '
+									<ul class="actions">
+										<li><a href="#" class="delete tips" data-tip="' . esc_attr__( 'Delete image', 'wp-restaurant-listings' ) . '">' . __( 'Delete', 'wp-restaurant-listings' ) . '</a></li>
+									</ul>
+								</li>';
+
+						// rebuild ids to be saved
+						$updated_gallery_ids[] = $attachment_id;
 					}
+
+					// need to update restaurant meta to set new gallery ids
+					if ( $update_meta ) {
+						update_post_meta( $post->ID, '_restaurant_image_gallery', implode( ',', $updated_gallery_ids ) );
+					}
+				}
 				?>
 			</ul>
 
@@ -79,7 +80,7 @@ class WP_Restaurant_Listings_Meta_Box_Gallery {
 	 * @param WP_Post $post
 	 */
 	public static function save( $post_id, $post ) {
-		$attachment_ids = isset( $_POST['restaurant_image_gallery'] ) ? array_filter( explode( ',',  $_POST['restaurant_image_gallery'] ) ) : array();
+		$attachment_ids = isset( $_POST['restaurant_image_gallery'] ) ? array_filter( explode( ',', $_POST['restaurant_image_gallery'] ) ) : array();
 
 		update_post_meta( $post_id, '_restaurant_image_gallery', implode( ',', $attachment_ids ) );
 	}
